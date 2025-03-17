f369c3b3276946bc94c4c622ebde7ee21057fa56
Benjamin <b.becker@iqb.hu-berlin.de>
2025-03-17 16:17:41 +0100

    add_data: Initialize empty GADSdat as oldrel

--- R/add_data.R
+++ R/add_data.R
@@ -1,6 +1,7 @@
 #' Add Data.
 #'
 #' Add the infrastructure for a new data set to the `eatDataTest` repository. This includes
+#' * creating an empty `GADSdat` object if `oldrel` is empty
 #' * a `.yaml` file
 #' * a markdown changelog (`.md` file)
 #' * a test file (`.R` file in `tests`)
@@ -23,6 +24,12 @@ add_data <- function(.path = getwd(), name, release_path, oldrel_path = "", vers
   # input validation
 
   file_path <- file.path(.path, "data", paste0(name, ".yaml"))
+
+  # create empty oldrel data set if now oldrel exists
+  if(identical(oldrel_path, "")) {
+    oldrel_path <- create_empty_oldrel(.path = .path, name = name)
+    cli::cli_alert_success("Created empty data set ('empty_gads.RDS') in data folder for initial diff.")
+  }
 
   # yaml
   cli::cli_par()
@@ -51,6 +58,20 @@ add_data <- function(.path = getwd(), name, release_path, oldrel_path = "", vers
   # TODO: create markdown entry for readme; how to paste this to clipboard?
 
   invisible(return(readme_snippet))
+}
+
+create_empty_oldrel <- function(.path = getwd(), name) {
+  # input validation
+
+  #browser()
+  empty_gads_path <- file.path(.path, "data/empty_gads.RDS")
+
+  if(!file.exists(empty_gads_path)) {
+    empty_gads_source <- system.file("extdata", "empty_gads.RDS", package = "eatDataTest")
+    file.copy(from = empty_gads_source, to = empty_gads_path)
+  }
+
+  return(empty_gads_path)
 }
 
 create_changelog <- function(.path = getwd(), name, version = "v1.0") {
