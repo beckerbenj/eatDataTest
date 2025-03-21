4f568420d60cda6e143f99eeb1283f5ae4e31bc2
Benjamin <b.becker@iqb.hu-berlin.de>
2025-03-21 18:10:31 +0100

    run_data_tests: Fixed icon production

--- R/run_data_tests.R
+++ R/run_data_tests.R
@@ -13,19 +13,19 @@
 #'@export
 run_data_tests <- function(.path = getwd(), name) {
   # input validation
-  #browser()
 
   test_path <- file.path(.path, "tests", paste0("test-", name, ".R"))
   icon_result_path <- file.path(.path, "tests", paste0("result-", name, ".svg"))
   test_out <- testthat::test_file(test_path)
+  test_out_df <- as.data.frame(test_out)
 
-  if(sum(test_out$failed == 1) == 0) {
+  if(sum(test_out_df$failed == 1) == 0) {
     # create pass icon
     icon_source <- system.file("extdata", "pass_icon.svg", package = "eatDataTest")
   } else {
     icon_source <- system.file("extdata", "fail_icon.svg", package = "eatDataTest")
   }
-  file.copy(from = icon_source, to = icon_result_path)
+  file.copy(from = icon_source, to = icon_result_path, overwrite = TRUE)
 
   invisible(return())
 }
--- tests/testthat/test-run_data_tests.R
+++ tests/testthat/test-run_data_tests.R
@@ -1,7 +1,6 @@
 
 
 test_that("create data diff", {
-  #browser()
   out <- capture_output_lines(run_data_tests(.path = test_path("helper_example_repo"), name = "helper_data1"))
 
   expect_false(grepl("FAIL 1 ", out[2]))
